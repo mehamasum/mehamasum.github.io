2206fd3c9563ed56149e851f2c86478c2cc14922
mehedi.masum <mehedi.masum@newscred.com>
2021-08-21 02:48:42 +0600

    Add links in client side

--- src/runtime.js
+++ src/runtime.js
@@ -47,4 +47,7 @@ TxtType.prototype.tick = function (firstRun = false) {
 window.onload = function () {
   const element = document.querySelector("#first-name");
   // new TxtType(element, ["Mehedi Hasan", "Meha"], 2000, 3);
+
+  document.getElementsByClassName('cryptedmail')[0].outerHTML = '<a class="subtext" href="mailto:mehamasum@gmail.com" title="Send Email">mehamasum@gmail.com</a>';
+  document.getElementsByClassName('cryptedtel')[0].outerHTML = '<a class="subtext" href="tel:+8801521112085" title="Phone">+880 152 111 2085</a>';
 };
